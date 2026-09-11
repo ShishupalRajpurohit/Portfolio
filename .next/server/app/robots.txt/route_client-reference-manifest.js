885a3559aa1ed502caba2186235512fbfdeb61b1
Shishupal Rajpurohit <shishupalrajpurohit2000@gmail.com>
2026-09-11 13:04:20 +0530

    Remove generated files from repository

--- .next/server/app/robots.txt/route_client-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-globalThis.__RSC_MANIFEST=(globalThis.__RSC_MANIFEST||{});globalThis.__RSC_MANIFEST["/robots.txt/route"]={"moduleLoading":{"prefix":"/_next/"},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"clientModules":{},"entryCSSFiles":{"C:\\Users\\sandi\\Downloads\\shishupal-portfolio-v2\\app\\robots.txt\\route":[]},"rscModuleMapping":{},"edgeRscModuleMapping":{}};
